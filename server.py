e98ffaf655106f59df7b9b17499cfe975fe2264a
Antony Ho <ntonyworkshop@gmail.com>
2018-01-08 01:44:16 +0800

    Import EIKON Data API python package
    Other updates:
    - Load EIKON API
    - Intall YAML to store configuration
    - Add PyYAML to virtualenv preparation

--- server.py
+++ server.py
@@ -2,9 +2,13 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from json import dumps
+import eikon as ek
+import yaml
 
 app = Flask(__name__)
 api = Api(app)
+conf = yaml.load(open('config.yml'))
+ek.set_app_id(conf['eikon']['apikey'])
 
 if __name__ == '__main__':
     app.run()
