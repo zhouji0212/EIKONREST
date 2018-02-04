7cff89fc972df9f884c4ecba1612950432e32553
Antony Ho <ntonyworkshop@gmail.com>
2018-02-04 23:35:27 +0800

    API key token support for the wrapper

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #!eikonrest/bin/python
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
-from json import dumps
+# from json import dumps
 import eikon as ek
 import yaml
 
@@ -13,17 +13,32 @@ ek.set_app_id(conf['eikon']['apikey'])
 
 class Query(Resource):
     def get(self, syms, fs, params=''):
-        # TODO - Check UUID in Bearer of API request
-        df = ek.get_data(instruments=str(syms).split(','), fields=str(fs).split(','), parameters=params, raw_output=True)
-        return jsonify(df)
+        #  Check UUID in Bearer of API request. This is a simple wrapper.
+        token = request.headers.get('Authorization', None)
+        if token is None:
+            return None
+        method, tokenstr = token.split()
+        if method.lower() == "bearer" and tokenstr == conf['pywrapper']['apikey']:
+            df = ek.get_data(instruments=str(syms).split(','), fields=str(fs).split(','), parameters=params,
+                             raw_output=True)
+            return jsonify(df)
+        else:
+            return None
 
 
 class ListHSIOptions(Resource):
     def get(self, fs, params=''):
-        # TODO - Check UUID in Bearer of API request
-        df = ek.get_data(instruments='0#HSI*.HF', fields=str(fs).split(','), parameters=params,
-                         raw_output=True)
-        return jsonify(df)
+        #  Check UUID in Bearer of API request. This is a simple wrapper.
+        token = request.headers.get('Authorization', None)
+        if token is None:
+            return None
+        method, tokenstr = token.split()
+        if method.lower() == "bearer" and tokenstr == conf['pywrapper']['apikey']:
+            df = ek.get_data(instruments='0#HSI*.HF', fields=str(fs).split(','), parameters=params,
+                             raw_output=True)
+            return jsonify(df)
+        else:
+            return None
 
 
 api.add_resource(Query, '/query/<string:syms>/<string:fs>', '/query/<string:syms>/<string:fs>/<string:params>')
